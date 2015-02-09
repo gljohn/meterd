d7826ff02ec02ff21ea2fb44a0f8394c5071ebef
gljohn <gljohn@fedoraproject.org>
2015-02-09 08:30:56 +0000

    tidying PEP-8

--- meterd/reading.py
+++ meterd/reading.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
-import weakref, logging
+import weakref
+import logging
+
+
 class Reading(object):
-        def __init__(self, parent, value=None, timestamp=None, logger=None):
-            self.parent = weakref.ref(parent) 
-            self.value = value
-            self.timestamp = timestamp
-            self.unit = parent.unit
-            self.uid = parent.uid
-            self.logger = logger or logging.getLogger(__name__)
 
-        def __str__(self):
-            return ( "value=" + str(self.value) + "&"
-                     + "time=" + str(self.timestamp) + "&"
-                     + "unit=" + str(self.unit) + "&"
-                     + "meterid=" + str(self.uid) )
+    def __init__(self, parent, value=None, timestamp=None, logger=None):
+        self.parent = weakref.ref(parent)
+        self.value = value
+        self.timestamp = timestamp
+        self.unit = parent.unit
+        self.uid = parent.uid
+        self.logger = logger or logging.getLogger(__name__)
 
-        def submit(self):
-            #DO HTTP SEND HERE.
-            self.logger.info(self)
+    def __str__(self):
+        return ("value=" + str(self.value) + "&"
+                + "time=" + str(self.timestamp) + "&"
+                + "unit=" + str(self.unit) + "&"
+                + "meterid=" + str(self.uid))
+
+    def submit(self):
+        #DO HTTP SEND HERE.
+        self.logger.info(self)
